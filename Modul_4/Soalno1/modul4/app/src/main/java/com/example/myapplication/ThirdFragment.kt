47e0abee67b44fba274cf9a7eddf14556f03f8c9
SitiNurAlfi <sitinuralfihidayah@gmail.com>
2023-05-15 01:12:32 +0700

    update new file

---
+++ Modul_4/Soalno1/modul4/app/src/main/java/com/example/myapplication/ThirdFragment.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment(),View.OnClickListener {
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.third_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val inputData: TextView = view.findViewById(R.id.value)
+        val btnCategory: ImageButton = view.findViewById(R.id.backButton)
+        val searchData = arguments?.getString("inputData")
+
+
+        inputData.text = searchData + " Not Found"
+
+
+        btnCategory.setOnClickListener(this)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CategoryFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ThirdFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onClick(p0: View) {
+        if(p0.id == R.id.backButton){
+            val secondFragment = SecondFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager
+                .beginTransaction()
+                .apply {
+                    replace(R.id.frame_container, secondFragment, SecondFragment::class.java.simpleName)
+                    addToBackStack(null)
+                    commit()
+                }
+        }
+    }
+}
